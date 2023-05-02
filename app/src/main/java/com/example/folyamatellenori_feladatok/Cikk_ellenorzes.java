8504acabbef7b4352cd057cbef52a4ae48744e2b
joeyzoli <joeyayeti@gmail.com>
2023-05-02 14:50:30 +0200

    kép mentés
    Már alakul a képem netés, beolvasom és elvileg tárolom is, holnap megírom a mentést hozzá

--- app/src/main/java/com/example/folyamatellenori_feladatok/Cikk_ellenorzes.java
+++ app/src/main/java/com/example/folyamatellenori_feladatok/Cikk_ellenorzes.java
@@ -1,29 +1,36 @@
 package com.example.folyamatellenori_feladatok;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +42,10 @@ public class Cikk_ellenorzes extends AppCompatActivity
     TextView idoo13;TextView idoo14;TextView idoo15;TextView idoo16;
     int letezik = 0;
     private static Object zar_4 = new Object();
+    ArrayList<String> kepnev = new ArrayList<>();
+    ArrayList<Uri> kephely = new ArrayList<>();
+    private Button buttonBrowse;
+    int SELECT_PICTURE = 200;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +76,16 @@ public class Cikk_ellenorzes extends AppCompatActivity
                 System.out.print(e);
             }
         }
+        buttonBrowse = (Button) findViewById(R.id.kep_gomb);
+        buttonBrowse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+                //onActivityResult(MY_REQUEST_CODE_PERMISSION, MY_RESULT_CODE_FILECHOOSER, this);
+            }
+
+        });
     }
 
     public void ujoldal_nxt(View view)
@@ -211,6 +232,37 @@ public class Cikk_ellenorzes extends AppCompatActivity
                 zar_4.notify();        // Értesítjük a zar_2-t, hogy mehet
             }
             return info;
+        }
+    }
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    //IVPreviewImage.setImageURI(selectedImageUri);
+                    kephely.add(selectedImageUri);
+                }
+            }
         }
     }
 
